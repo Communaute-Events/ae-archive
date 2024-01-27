b6b3f5861d17a5137da3cebfd59274f30a275380
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-01-27 14:44:25 +0100

    Added experimental automatic builds workflow

--- building/patch.sh
+++ building/patch.sh
@@ -2,3 +2,5 @@
 # This file patches some dev environement things
 
 sed -i '' 's/http:\/\/localhost:[0-9]*//g' svelte/index.html
+echo -e "\x1b[32;1mPatched 'svelte/index.html\x1b[0m'"
+rm -rf dist/*
