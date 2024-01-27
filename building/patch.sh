fa21d05eb9e477c04e9c4a7960e3bdaec3859f6a
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-01-27 15:03:42 +0100

    Trying to fix sed on linux

--- building/patch.sh
+++ building/patch.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # This file patches some dev environement things
 
-sed -i '' 's/http:\/\/localhost:[0-9]*//g' svelte/index.html
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    sed -i '' 's/http:\/\/localhost:[0-9]*//g' svelte/index.html
+else
+    # Linux
+    sed -i 's/http:\/\/localhost:[0-9]*//g' svelte/index.html
+fi
 echo -e "\x1b[32;1mPatched 'svelte/index.html\x1b[0m'"
-rm -rf dist/*
