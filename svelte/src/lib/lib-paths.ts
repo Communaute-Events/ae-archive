10e4382bf9ebac2c3329e74f52c173a19aa088be
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-02-04 02:49:52 +0100

    Implemented Notifications for MacOS

---
+++ svelte/src/lib/lib-paths.ts
@@ -0,0 +1,10 @@
+const LibPaths = {
+    notifications: {
+        darwin: {
+            prod: window.NL_PATH + "/lib/alerter",
+            dev: window.NL_PATH + "/building/_app_scaffolds/mac/myapp.app/Contents/Resources/lib/alerter"
+        }
+    }
+}
+
+export default LibPaths
