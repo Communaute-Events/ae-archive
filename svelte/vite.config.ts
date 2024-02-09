c1cd789628c528d2e48dffb7e18438cd6db17b03
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-02-09 16:05:29 +0100

    Added TailwindCSS and shadcn-svelte

--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import Terminal from 'vite-plugin-terminal'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // @ts-expect-error
   plugins: [svelte(),Terminal()],
   build: {
     rollupOptions: {
       external: [
         '/_neutralino_globals.js'
       ]
+    }
+  },
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib")
     }
   }
 })
